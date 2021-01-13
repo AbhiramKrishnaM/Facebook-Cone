279214639f29e3c2af7486c19fcc8e1665f6cd6d
AbhiramKrishna <abhiramKrishna.8921@gmail.com>
2021-01-13 19:22:57 +0530

    post component added : mobile responsive not created

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import StoryReel from './StoryReel';
 import MessageSender from './MessageSender';
+import Post from './Post';
+
 import '../Css/Feed.css';
 function Feed() {
     return (
         <div className="feed">
             <StoryReel />
-            {/* message sender */}
             <MessageSender /> 
+            <Post profilePic="https://www.imagesource.com/wp-content/uploads/2019/06/Rio.jpg" message ="facebook !!" timestamp="this is timeStamp" username="AbhiramKrishna" image='https://i.pinimg.com/originals/af/8d/63/af8d63a477078732b79ff9d9fc60873f.jpg' />
+            <Post profilePic="https://analyticsindiamag.com/wp-content/uploads/2018/12/image.jpg" message ="facebook !!" timestamp="this is timeStamp" username="Anumodh" image='https://dyl80ryjxr1ke.cloudfront.net/external_assets/hero_examples/hair_beach_v1785392215/original.jpeg' />
+            <Post profilePic="https://i0.wp.com/www.oakridge.in/wp-content/uploads/2020/02/Sample-jpg-image-500kb.jpg" message ="facebook !!" timestamp="this is timestamp" username="adith" image='https://cdn.searchenginejournal.com/wp-content/uploads/2019/08/c573bf41-6a7c-4927-845c-4ca0260aad6b-760x400.jpeg' />
         </div>
     )
 }
--- src/Components/MessageSender.js
+++ src/Components/MessageSender.js
@@ -1,22 +1,31 @@
 import { Avatar } from '@material-ui/core';
-import React from 'react'
+import React, { useState } from 'react'
 import '../Css/MessageSender.css';
 import VideoCallIcon from '@material-ui/icons/VideoCall';
 import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 
 function MessageSender() {
+
+    const [input, setInput] = useState('')
+    const [imageUrl, setImageUrl] = useState('');
+    
     const handleSubmit = e => {
         // to stop refreshing when user clicks  
         e.preventDefault();
+
+        // database functionality
+
+        setInput('');
+        setImageUrl('');
     }
     return (
         <div className ="messageSender">
             <div className="messageSender__top">
                 <Avatar />
                 <form>
-                    <input className="messageSender__input" placeholder={`what's on your mind ?`}></input>
-                    <input placeholder="image URL (Optional)"></input>
+                    <input value={input} onChange={(e)=> setInput(e.target.value)} className="messageSender__input" placeholder={`what's on your mind ?`}></input>
+                    <input value={imageUrl} onChange={(e)=> setImageUrl(e.target.value)} placeholder="image URL (Optional)"></input>
                     <button onClick={handleSubmit} type="submit">Hidden submit</button>
                 </form>
             </div>

4de1a09b08a7468fe08be29ae753e5e89cef0c96
AbhiramKrishna <abhiramKrishna.8921@gmail.com>
2021-01-13 21:09:03 +0530

    Added google authentification: mobile responsiveness not implemented

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,26 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
-import '../Css/login.css';
+import { auth, provider } from "../Firebase/Firebase";
+import '../Css/Login.css';
 function Login() {
+    const signIn = ()=>{
+        // sign in
+        auth.signInWithPopup(provider)
+        .then(result => {
+            console.log(result);
+        }).catch(error => alert(error.message));
+    }
     return (
         <div className="login"> 
-            
+            <div className="login__logo">
+                <img 
+                src="https://1000logos.net/wp-content/uploads/2016/11/Facebook-logo-500x350.png"
+                alt=""></img>
+                <img
+                src="https://katanningrotary.org/wp-content/uploads/2019/04/facebook-text-transparent-logo-23.png"
+                alt=""></img>
+            </div>
+            <Button type="submit" onClick={signIn}>Sign In</Button>
         </div>
     )
 }
--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyA4oTMSHbDv-qtF8b8-93ZybwXWjTh7vR4",
     authDomain: "facebook-clone-583e5.firebaseapp.com",
@@ -6,3 +8,11 @@ const firebaseConfig = {
     messagingSenderId: "195034274812",
     appId: "1:195034274812:web:197b9f76c866b22ad1f079"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export {auth , provider};
+  export default db;
